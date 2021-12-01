7511ce771e8ac3fe3a575b457815469e114120ad
Fernando Azevedo <azevedo-fernando@users.noreply.github.com>
2021-12-01 13:04:35 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path') // Utilizado para substituir o 'barra' / ou \ que é diferente nos SO - __dirname vai pegar o diretório do arquivo da instrução.
+const HtmlWebpcakPlugin = require('html-webpack-plugin')
+
+const isDevelepment = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+    mode: isDevelepment ? 'development' : 'production',
+    devtool: isDevelepment ? 'eval-source-map' : 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        static: path.join(__dirname, 'public')
+    },
+    plugins: [
+        new HtmlWebpcakPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader'],
+            }
+        ],
+    }
+}
