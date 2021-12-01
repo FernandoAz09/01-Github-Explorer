3d378c76055d84ada619789a86b7a113952b7290
Fernando Azevedo <azevedo-fernando@users.noreply.github.com>
2021-12-01 13:43:30 -0300

    Criando um Componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
+/*Componente em React =  Dividir o app em varios pedaços
+ é uma função *com a primeira letra do nome SEMPRE em maiusculo*
+ um pcomponente sempre por arquivo
+ que devolve um HTML*/ 
 export function App() {
-    return <h1>Hello ReactJS!</h1>
+    return <RepositoryList/>
 }
